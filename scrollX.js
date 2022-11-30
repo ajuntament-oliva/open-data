5a5001fabc46e626ca65a079b3b163da5cd78326
ajuntament-oliva <informatica@oliva.es>
2022-11-30 13:13:38 +0100

    feat: :arrow_up:

---
+++ scrollX.js
@@ -0,0 +1,6 @@
+const button=documement.createElement('button');
+button.style.display = 'none';
+button.style.position = 'fixed';
+button.style.left ="90%";
+button.style.bottom ="10%";
+document.appendChild(button);
